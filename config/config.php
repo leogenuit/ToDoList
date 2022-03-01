f3dd6c1ceb26a2e0b6f8cf6d0f36f1c539dfc69d
Leo GENUIT <genuitleo@gmail.com>
2022-03-01 19:34:04 +0100

    commit gitignore

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+
+$dbname = "test";
+$pseudo = "root";
+$pwd = "";
--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-
-  $db = new PDO('mysql:host=localhost;dbname=ToDoListrReal', "root", "root");
-} catch (Exception $e) {
-  die('Erreur : ' . $e->getMessage());
-}
-  ?>
