33b45867ae72b8d51fdcee98f7a9337d1ed496ee
Leo GENUIT <genuitleo@gmail.com>
2022-03-03 19:34:38 +0100

    add delete

--- index.php
+++ index.php
@@ -6,7 +6,9 @@ if (isset($_POST['add'])) {
   $addContent = $db->prepare("INSERT INTO to_do(title) VALUE(:title)");
   $addContent->execute(['title' => $_POST['title']]);
 }
-
+if (isset($_POST['delete'])) {
+// $deleteContent = $db->prepare("DELETE FROM "to_do" WHERE ")
+}
 $req_all_to_do = $db->prepare("SELECT * FROM to_do");
 $req_all_to_do->execute();
 
